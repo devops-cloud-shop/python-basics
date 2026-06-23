4085a809a4c663ec3dd19db77a51fb8cecb2a680
Prav-Akula <pravs.akula@outlook.com>
2026-06-23 13:10:16 -0400

    python

--- 07-set.py
+++ 07-set.py
@@ -1,3 +1,14 @@
 s1 = set()
-sample = {'s', 'd', 'f'}
+sample = {'s', 'd', 'f', 'd'}
 print(sample)
+
+#set is an unordered collection
+# sets remove duplicates and stores unique values
+
+# print(dir(set()))
+"""
+['add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset', 'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 'union', 'update']
+"""
+s1 = { 1,35,4,3,28}
+s2 = {1,25,4,3}
+print(s1.difference(s2))
