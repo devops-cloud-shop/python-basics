39b9217bcf314a4f3999f138fcfb472098d5bd25
Prav-Akula <pravs.akula@outlook.com>
2026-06-23 11:56:07 -0400

    python

--- 06-string.py
+++ 06-string.py
@@ -1,2 +1,12 @@
 sample = "abd"
 print(sample)
+
+# sample[0] = 'h'
+# print(sample)
+
+#string doesnot support item assignment - means its a immutable datatype
+# just like tuple and dict
+
+print(dir('string'))
+['capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+
