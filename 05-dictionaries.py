2fe56f8e45adf24ca904d1f8abe23f1e7b2bf091
Prav-Akula <pravs.akula@outlook.com>
2026-06-23 12:26:03 -0400

    python

--- 05-dictionaries.py
+++ 05-dictionaries.py
@@ -12,3 +12,11 @@ print(sample['a']) # a value can be modified
 
 # Value can be modified But Keys cannot be modified
 
+#example
+sample = {('a' , 'b'): 18, 'd' : 24}
+print(type(sample), sample)
+
+# print(dir(dict))
+"""
+['clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 'setdefault', 'update', 'values']
+"""
